fc1898978ee4f1fdd059a078a3c96cb7c68ef431
unknown <pavluha2dva8@gmail.com>
2020-12-09 12:11:56 +0200

    some styles for Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,29 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import style from './Navbar.module.css'
 
 const Navbar = () => {
-    return <nav className={style.nav}>
-    <ul>
-        <li className={style.nav__list}> <a href="/profile" className={style.nav__item}>Profile</a></li>
-        <li className={style.nav__list}> <a href="/dialogs" className={style.nav__item}>Messages</a></li>
-        <li className={style.nav__list}> <a href="/news" className={style.nav__item}>News</a></li>
-        <li className={style.nav__list}> <a href="/music" className={style.nav__item}>Music</a></li>
-        <li className={style.nav__list}> <a href="/settings" className={style.nav__item}>Settings</a></li>
-    </ul>
-</nav>
+    return (
+        <nav className={style.nav}>
+            <ul>
+                <li className={style.nav__list}>
+                    <NavLink to="/profile" className={style.nav__item} activeClassName={style.nav__item_active}>Profile</NavLink>
+                </li>
+                <li className={style.nav__list}>
+                    <NavLink to="/dialogs" className={style.nav__item} activeClassName={style.nav__item_active}>Messages</NavLink>
+                </li>
+                <li className={style.nav__list}>
+                    <NavLink to="/news" className={style.nav__item} activeClassName={style.nav__item_active}>News</NavLink>
+                </li>
+                <li className={style.nav__list}>
+                    <NavLink to="/music" className={style.nav__item} activeClassName={style.nav__item_active}>Music</NavLink>
+                </li>
+                <li className={style.nav__list}>
+                    <NavLink to="/settings" className={style.nav__item} activeClassName={style.nav__item_active}>Settings</NavLink>
+                </li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Navbar
